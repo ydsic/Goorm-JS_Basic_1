a8e6575894db472db4d35c45675bd62f0b104197
ydsic <ydsic99@gmail.com>
2025-03-13 13:46:23 +0900

    피드백으로 인한 코드 수정

--- index.js
+++ index.js
@@ -42,68 +42,79 @@ function hideAllContents() {
 }
 
 // nav 태그에서 버튼을 클릭 했을 때 section을 보여주는 함수
-function showContent1() {
-  // 클릭이 안된 나머지 section들은 hideAllContents() 함수를 먼저 실행
+function showContent(missionId) {
+    // 클릭이 안된 나머지 section들은 hideAllContents() 함수를 먼저 실행
   hideAllContents();
-  // 그리고 mission1이란 id를 가진 section을 변수에 저장시키고
-  const mission = document.getElementById('mission1');
+    // 그리고 mission1이란 id를 가진 section을 변수에 저장시키고
+  const mission = document.getElementById(missionId);
 
-  // display가 none으로 되어있던걸 flex로 바꾸는데
+    // display가 none으로 되어있던걸 flex로 바꾸는데
   mission.style.display = 'flex';
-
-  // setTimeout으로 아주 잠깐 지연시킨 뒤 투명도를 1초동안 서서히 1이 되게 효과를 준다
+    // setTimeout으로 아주 잠깐 지연시킨 뒤 투명도를 1초동안 서서히 1이 되게 효과를 준다
   setTimeout(() => {
     mission.style.opacity = '1';
     mission.style.transition = 'opacity 1000ms';
   }, 100);
-
-  // 추가설명 : setTimeout을 주지 않으면 display를 none에서 flex로 변경되면서 display가 로딩이 되는 동안 opacity가 처리가 되면서
+    // 추가설명 : setTimeout을 주지 않으면 display를 none에서 flex로 변경되면서 display가 로딩이 되는 동안 opacity가 처리가 되면서
   // 화면에 아주 잠깐동안 보이게 되므로 서서히 투명도가 올라가는 부드러운 효과를 줄 수 없기 때문
 }
 
-// 아래부터는 showContent1() 이랑 같은 코드이지만 class name만 다릅니다.
-// 코드길이를 줄이고 더 간편한 방식으로 코드를 짤 수 있었지만 아직 DOM 사용이 어색한데 알고리즘 부분까지 생각하려니 모르겠어서..
-function showContent2() {
-  hideAllContents();
-  const mission = document.getElementById('mission2');
 
-  mission.style.display = 'flex';
-  setTimeout(() => {
-    mission.style.opacity = '1';
-    mission.style.transition = 'opacity 1000ms';
-  }, 100);
-}
 
-function showContent3() {
-  hideAllContents();
-  const mission = document.getElementById('mission3');
+// 아래 코드는 피드백 받기 전에 코드인데 DOM 사용이 익숙치 않은 상태에 알고리즘까지 생각해보려니 헷갈려서 비효율적으로 코드를 짜고 피드백을 보냈었는데
+// id값을 매개변수로 받아오면 코드를 하나로 통일시킬 수 있다는 피드백을 받고 위에 코드로 수정했습니다
 
-  mission.style.display = 'flex';
-  setTimeout(() => {
-    mission.style.opacity = '1';
-    mission.style.transition = 'opacity 1000ms';
-  }, 100);
-}
+// function showContent1() {
+//   hideAllContents();
+//   const mission = document.getElementById('mission1');
 
-function showContent4() {
-  hideAllContents();
-  const mission = document.getElementById('mission4');
+//   mission.style.display = 'flex';
+//   setTimeout(() => {
+//     mission.style.opacity = '1';
+//     mission.style.transition = 'opacity 1000ms';
+//   }, 100);
+// }
 
-  mission.style.display = 'flex';
-  setTimeout(() => {
-    mission.style.opacity = '1';
-    mission.style.transition = 'opacity 1000ms';
-  }, 100);
-}
+// function showContent2() {
+//   hideAllContents();
+//   const mission = document.getElementById('mission2');
 
-function showContent5() {
-  hideAllContents();
-  const mission = document.getElementById('mission5');
+//   mission.style.display = 'flex';
+//   setTimeout(() => {
+//     mission.style.opacity = '1';
+//     mission.style.transition = 'opacity 1000ms';
+//   }, 100);
+// }
 
-  mission.style.display = 'flex';
-  setTimeout(() => {
-    mission.style.opacity = '1';
-    mission.style.transition = 'opacity 1000ms';
-  }, 100);
-}
+// function showContent3() {
+//   hideAllContents();
+//   const mission = document.getElementById('mission3');
 
+//   mission.style.display = 'flex';
+//   setTimeout(() => {
+//     mission.style.opacity = '1';
+//     mission.style.transition = 'opacity 1000ms';
+//   }, 100);
+// }
+
+// function showContent4() {
+//   hideAllContents();
+//   const mission = document.getElementById('mission4');
+
+//   mission.style.display = 'flex';
+//   setTimeout(() => {
+//     mission.style.opacity = '1';
+//     mission.style.transition = 'opacity 1000ms';
+//   }, 100);
+// }
+
+// function showContent5() {
+//   hideAllContents();
+//   const mission = document.getElementById('mission5');
+
+//   mission.style.display = 'flex';
+//   setTimeout(() => {
+//     mission.style.opacity = '1';
+//     mission.style.transition = 'opacity 1000ms';
+//   }, 100);
+// }
